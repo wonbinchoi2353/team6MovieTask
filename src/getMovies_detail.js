317aee3db2fccebae765dc8453de0bb1008e13c9
leeseungjun <cjstkrak@gmail.com>
2023-06-06 16:13:45 +0900

    디테일에 있는 정보 출력

--- src/getMovies_detail.js
+++ src/getMovies_detail.js
@@ -1,14 +1,29 @@
+// export { getMovieDetail };
 
-let movieId;
-const options = {
-method: 'GET',
-headers: {
-accept: 'application/json',
-Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYjdmNWM2OGZkY2VhMzJhYTZkOTEzZWE3YThlODNmZCIsInN1YiI6IjY0NzA5NDMzMTNhMzIwMDEzMzg2MGQ5MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.H6E-QYJymel6CZVaT0YDHYLWFIctLiVqU1Qv-7gNqe0'
+async function getMovieDetail() {
+    let movie_details
+
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYjdmNWM2OGZkY2VhMzJhYTZkOTEzZWE3YThlODNmZCIsInN1YiI6IjY0NzA5NDMzMTNhMzIwMDEzMzg2MGQ5MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.H6E-QYJymel6CZVaT0YDHYLWFIctLiVqU1Qv-7gNqe0'
+        }
+    };
+    try {
+        movie_details = await fetch('https://api.themoviedb.org/3/movie/${movieId}?language=en-US', options)
+        // 패치로 가져온 데이터를 movies에 할당
+    } catch (error) {
+        alert('오류'); // 오류시 오류알림
+    }
+    return movie_details.json() // 받은 데이터를 json 형식으로 반환
 }
-};
 
-fetch('https://api.themoviedb.org/3/movie/${movieId}?language=en-US', options)
-.then(response => response.json())
-.then(response => console.log(response))
-.catch(err => console.error(err));
+
+async function renderMovies() {  /* 패치로 가져온 데이터를 찍는 과정 */
+    let detail = await getMovieDetail(); // 객체구조분해할당 방식으로 변수저장을 해줘야한다. getMovie()로 가져온 데이터는 배열이기 때문에 객체로 변환
+    console.log(detail)
+       // setpage(movies) // movies 라는 객체를 받아서 화면에 출력하는 함수
+}
+
+renderMovies()
