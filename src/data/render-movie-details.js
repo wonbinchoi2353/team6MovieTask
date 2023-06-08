0b4efadf0b07eddf8971cc534898f1397cd16101
wonbin choi <fnffnqls@naver.com>
2023-06-09 00:58:13 +0900

    수정, 삭제 기능 만드는 중

--- src/data/render-movie-details.js
+++ src/data/render-movie-details.js
@@ -6,7 +6,7 @@ async function renderMovieDetails() {
   let html = '';
 
   html = `<li><img src="https://image.tmdb.org/t/p/w500${movie_detail.backdrop_path}">
-    <li><h2>✅제목 : ${movie_detail.title}</h2></li>
+    <li><h2 class="detailPageTitle">✅제목 : ${movie_detail.title}</h2></li>
 <li><h2>${movie_detail['tagline']}</h2></li>
 <li><h2>상영시간 : ${movie_detail.runtime}분</h2></li>
 <li><h2> 평점 : ${movie_detail.vote_average}</h2></li>
