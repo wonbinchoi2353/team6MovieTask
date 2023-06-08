bf4947963e6d8270b0fa4feec24651e65ff2ebb2
leeseungjun <cjstkrak@gmail.com>
2023-06-08 10:04:52 +0900

    html주석

--- src/data/get-id.js
+++ src/data/get-id.js
@@ -1,8 +1,7 @@
-export { getId }
-
-const getId = function (e) {
+const getId = function (e) { // 누른 영화박스의 Id를 수집
     let dv = e.target
     let a = dv.parentNode.id
     return a 
-    // showcur(a)
 }
+
+export { getId }
