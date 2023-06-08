1fecec0a150145689d82bd9b5ab3d5697611017f
leeseungjun <cjstkrak@gmail.com>
2023-06-09 04:00:26 +0900

    삭제기능완성

--- src/detail/data/render-review.js
+++ src/detail/data/render-review.js
@@ -21,9 +21,42 @@ function renderReview() {
         <div><input class="review-Writer" value="${postReviewWriter}"></input></div>
         <div><input class="review-Text" value="${postReviewContent}"></input></div>
         <button class="modify-Btn">수정</button>
+        <p><label for="">확인비밀번호</label>
+        <input type="text" class="checkPwd"></p>
         <button class="delete-Btn">삭제</button>
         </div>`;
   });
 }
 
-export { renderReview, reviews, movieId };
+function saveReview() {
+  // 인풋값 가져오기
+  let reviewWriter = document.querySelector('.writer').value;
+  let reviewPassword = document.querySelector('.password').value;
+  let reviewContent = document.querySelector('.review-text').value;
+  // 유저 dateId
+  let dateId = new Date().getTime();
+  // 인풋값이 없으면 리뷰 저장 취소
+  if ([reviewWriter, reviewPassword, reviewContent].includes('')) {
+    alert('비어있는 내용을 채워주세요!');
+    return;
+    // 로컬 스토리지에 리뷰가 있으면
+  } else if (localStorage.length === 1) {
+    // 로컬 스토리지에 있는 리뷰 꺼내기
+    reviews = localStorage.getItem('reviews');
+    reviews = JSON.parse(reviews);
+    // 로컬 스토리지에 아무것도 없으면
+  }
+  // 인풋값 배열에 추가하기, 단축 속성명?
+  reviews.push({
+    movieId,
+    dateId,
+    reviewWriter,
+    reviewPassword,
+    reviewContent,
+  });
+  // 리뷰 배열을 문자열로 만들어서 localStorage에 저장
+  reviews = JSON.stringify(reviews);
+  localStorage.setItem('reviews', reviews);
+}
+
+export { renderReview, reviews, movieId, saveReview };
--- src/detail/detail-main.js
+++ src/detail/detail-main.js
@@ -1,32 +1,23 @@
 import { renderMovieDetails } from './data/render-movie-details.js';
-import { renderReview, movieId } from './data/render-review.js';
-import { saveReview } from './data/save-review.js';
-renderMovieDetails();
+import { renderReview, saveReview } from './data/render-review.js';
+import { deleteReview } from './utill/delete.js';
 
-document.querySelector('#home-btn').addEventListener('click', function () {
-  window.location.href = 'index.html';
-});
-document.querySelector('.review-btn').addEventListener('click', function () {
-  saveReview();
-  renderReview();
-  location.reload();
-});
-window.onload = () => {
+
+window.onload = () => { // 페이지가 켜지면 상세페이지와 리뷰
+  renderMovieDetails();
   if (localStorage.length >= 1) {
     renderReview();
   }
 };
+document.querySelector('#home-btn').addEventListener('click', function () { // index.html로 돌아가기
+  window.location.href = 'index.html';
+});
+document.querySelector('.review-btn').addEventListener('click', function () { // 저장버튼 누르면 리뷰저장 및 다시 렌더링
+  saveReview();
+  renderReview();
+  location.reload();
+});
 
-document.querySelector('.review-list').addEventListener('click', function (e) {
-  //클릭한 대상이 add라는 클래스를 가지고있다면
-  if (e.target.classList.contains('delete-Btn')) {
-    console.log('발견했다');
-    let reviews = localStorage.getItem('reviews');
-    reviews = JSON.parse(reviews);
-    // 이벤트 타겟으로 클릭한 button의 부모 요소 제외하고 선택 (이렇게 쓰는 게 맞는지 정확히는 모르겠습니다..)
-    reviews = reviews.filter(review => review.dateId !== e.target.parentElement.id);
-    // 삭제한 리뷰 빼고 다시 로컬 스토리지에 저장
-    reviews = JSON.stringify(reviews);
-    localStorage.setItem('reviews', reviews);
-  }
+document.querySelector('.review-list').addEventListener('click', function (e) { // 삭제
+  deleteReview(e);
 });
