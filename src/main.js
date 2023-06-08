bac896fce402dca9ae5fa5826dccf025e7b1e7ae
wonbin choi <fnffnqls@naver.com>
2023-06-08 12:53:26 +0900

    dev, wonbin merge

--- src/main.js
+++ src/main.js
@@ -9,6 +9,10 @@ renderMovies(); // 동작
 // Uncaught ReferenceError: search is not defined
 //     at HTMLButtonElement.onclick
 
-document.querySelector('#search-btn').addEventListener('click', search)
-document.querySelector('#search-btn2').addEventListener('click', sort)
-document.addEventListener('keydown', onKeyUp)
+document.querySelector('#search-btn').addEventListener('click', search);
+document.querySelector('#search-btn2').addEventListener('click', sort);
+document.addEventListener('keydown', onKeyUp);
+document.querySelector('.card-list').addEventListener('click', function (e) {
+  const id = getId(e);
+  window.location.href = `./detail.html?movie_id=${id}`;
+});
--- src/onKeyup.js
+++
@@ -1,9 +0,0 @@
-import { search } from './serch.js';
-export { onKeyUp };
-
-function onKeyUp(e) {
-  // enter키 입력시 검색
-  if (e.keyCode == 13) {
-    search();
-  }
-}
