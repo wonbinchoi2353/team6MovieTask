224239ec416b0a15f00f4a6456fd5ae44848552e
leeseungjun <cjstkrak@gmail.com>
2023-06-08 18:05:18 +0900

    detail로 review 합침

--- src/data/render-movie-details.js
+++ src/data/render-movie-details.js
@@ -7,11 +7,11 @@ async function renderMovieDetails() {
   console.log(movie_detail)
 
   html = `<img src="https://image.tmdb.org/t/p/w500${movie_detail.backdrop_path}">
-    <li><h1>✅${movie_detail.title}</h1></li>
+    <li><h1>${movie_detail.title}</h1></li>
     <li><h2>✅original title : ${movie_detail.original_title}</h2></li>
     <li><h3>${movie_detail['tagline']}</h2></li>
     <li><h4>장르 : ${movie_detail.genres[0].name}</h3></li>
-<li><h4>상영시간 : ${movie_detail.runtime}분</h2></li>
+<li><h4>⏳상영시간 : ${movie_detail.runtime}분</h2></li>
 <li><h4>⭐${movie_detail.vote_average}</h2></li>
 <a href="${movie_detail.homepage}">🏠Home Page</a>
 <p>개봉일 : ${movie_detail.release_date}</p>
