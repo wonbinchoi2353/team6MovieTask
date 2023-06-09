bf5446676c39a408f43e7ddd2b97267afb1f216c
leeseungjun <cjstkrak@gmail.com>
2023-06-09 13:44:32 +0900

    오류잡자

--- src/detail/utill/delete-review.js
+++ src/detail/utill/delete-review.js
@@ -2,10 +2,12 @@ const deleteReview = e => { // 클릭 매개변수 e를 받아옴
   if (e.target.classList.contains('delete-Btn')) { // 누른 타겟이 delete-Btn 일 경우 실행
     let reviews = localStorage.getItem('reviews');
     reviews = JSON.parse(reviews);
-    let inputPwd = document.querySelector('.checkPwd').value; // 입력한 비밀번호 value
+    let inputPwd = e.target.nextElementSibling.children[0].value// 입력한 비밀번호 value
     let check = reviews.filter(review => review.dateId === Number(e.target.parentElement.id))
     // 삭제버튼을 누른 객체와 같은 객체를 데이터베이스에서 호출
     let originPwd = check[0].reviewPassword; // 호출한 객체의 비밀번호를 변수에 저장
+    console.log(originPwd)
+    console.log(inputPwd)
     if (originPwd === inputPwd) { // 객체 비밀번호와 입력한 비밀번호가 같으면 실행
       reviews = reviews.filter(review => review.dateId !== Number(e.target.parentElement.id));
       // 삭제한 리뷰 빼고 다시 로컬 스토리지에 저장
